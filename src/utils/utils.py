41f1ac03a17a1d2c6d16de442d3d37bc09359ec6
Carles Pina i Estany <carles@pina.cat>
2018-06-06 20:46:37 +0100

    Changes order functions utils.py

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,7 +1,25 @@
 import src.utils.positions as positions
 
 
-def text_for_position(col, row, solution, penguins):
+def print_solution(solution, penguins):
+    """
+    Prints the solution. A different letter for each piece type and P for penguins.
+    """
+    row = 0
+    while row < positions.board_size[1]:
+        col = 0
+        text_row = "   {}     {}".format(_text_for_position(col + 1, row, solution, penguins),
+                                         _text_for_position(col + 3, row, solution, penguins))
+        print(text_row)
+        text_row = "{}     {}     {}".format(_text_for_position(col, row, solution, penguins),
+                                             _text_for_position(col + 2, row, solution, penguins),
+                                             _text_for_position(col + 4, row, solution, penguins))
+        print(text_row)
+
+        row += 1
+
+
+def _text_for_position(col, row, solution, penguins):
     """
     Given col and row and the solution returns a letter for the piece,
     P for penguins and " " for possible blank squares (some challenges
@@ -19,7 +37,7 @@ def text_for_position(col, row, solution, penguins):
         if position in piece:
             allocated_piece = piece
 
-    if allocated_piece == None:
+    if allocated_piece is None:
         return " "
 
     for name in positions.all_dictionary.keys():
@@ -27,24 +45,6 @@ def text_for_position(col, row, solution, penguins):
             return name
 
     raise Exception("Piece not in the piece list?")
-
-
-def print_solution(solution, penguins):
-    """
-    Prints the solution. A different letter for each piece type and P for penguins.
-    """
-    row = 0
-    while row < positions.board_size[1]:
-        col = 0
-        text_row = "   {}     {}".format(text_for_position(col+1, row, solution, penguins),
-                                    text_for_position(col+3, row, solution, penguins))
-        print(text_row)
-        text_row = "{}     {}     {}".format(text_for_position(col, row, solution, penguins),
-                                        text_for_position(col+2, row, solution, penguins),
-                                        text_for_position(col+4, row, solution, penguins))
-        print(text_row)
-
-        row += 1
 
 
 if __name__ == "__main__":
